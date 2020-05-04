1251b3b3f5130eb3362004dee8421afff490a9c0
Kary01 <anakaren.ruizb@gmail.com>
2020-05-04 01:51:34 -0500

    ciclo while/do while

---
+++ 07-practica.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Kary Ruiz">
+    <meta name="description" content="Página de practica sobre bucles while">
+    <title>Practica 07</title>
+</head>
+<body>
+    <header>
+        <h1>BUCLES WHILE EN PHP</h1>
+    </header>
+    <main>
+
+<!-- Código PHP -->
+    <?php
+        /*Cuando se desee un repetir un mismo bloque de código
+            resulta más eficiente utilizar bucles while. Si la condicion
+            nunca se vuelve falsa, la instrucción se seguirá ejecutando indefinidamente*/
+            $i = 0;
+
+            while ($i <= 10) {
+                echo "<p>El valor es: $i<br /></p>"; //código a ejecutar si se cumple la condición
+                $i++;
+            }
+
+        /*El ciclo do while siempre ejecutará el código una vez*/
+        $a = 8;
+
+        do {
+            echo "<p>El número es: $a<br /></p>";
+            $a++;
+        } while ($a <= 16);
+    ?>
+<!-- /.. -->
+
+    </main>
+</body>
+</html>
