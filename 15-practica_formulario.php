78bcd041262866138070b9eff3b499950606d3a7
Kary01 <anakaren.ruizb@gmail.com>
2020-05-08 00:22:43 -0500

    ejemplo-formulario

--- 15-practica_formulario.php
+++ 15-practica_formulario.php
@@ -13,9 +13,10 @@
         <p>
             Bienvenido <?php echo $_POST['firstname']." ".$_POST['lastname']; ?><br></p>
             Tienes <?php echo $_POST['age']." años de edad"; ?><br>
-            Tu situción sentimental: <?php echo $_POST['situation']; ?><br>
+            Tu situción sentimental: <?php echo $_POST['drink']; ?><br>
             Teléfono: <?php echo $_POST['tel']; ?><br>
             Email: <?php echo $_POST['email']; ?><br>
+            Your comments: <?php echo $_POST['comments']; ?><br>
         </p>
     </main>
 </body>
