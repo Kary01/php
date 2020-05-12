430e86bfce87aea04e412997b8586cdfa1316a9c
Kary01 <anakaren.ruizb@gmail.com>
2020-05-12 14:52:51 -0500

    herencia

---
+++ 26-practica.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Las clases pueden heredar las propiedades y métodos 
+    de otras clases, las clases que heredan esas propiedades y métodos
+    se llaman 'subclases' y la clase de donde se heredan se llama 'clase padre' */
+
+
+    //Clase Padre
+    class Fruit{
+        public $name;
+        public $color;
+        public $weight;
+
+        public function favoriteFruit($name, $color, $weight){
+            echo "Your favorite fruit is: " .$this->name = $name. "<br>";
+            echo "The color is: " .$this->color = $color. "<br>";
+            echo "The weight is: " .$this->weight = $weight. "<br>";
+        }
+    }
+
+    echo "<h1>HERENCIA! CLASE PADRE: FRUTA FAVORITA</h1>";
+
+    $fruit1 = new Fruit();
+
+    $fruit1->favoriteFruit("Mango", "Yellow", "300");
+
+
+    //Subclase
+    class Otherfruit extends Fruit{ //Para acceder a la herencia se usa 'extends'
+        
+    }
+
+    echo "<h2>SUBCLASE: OTRA FRUTA FAVORITA</h2>";
+
+    $fruit2 = new Otherfruit();
+    $fruit2->favoriteFruit("Apple", "Red", "200");
+
+    /* La subclase 'Otherfruit' hereda todas las propiedades y métodos 
+    de la clase padre 'Fruit'*/
+    
+?>
