0178731e60792f7ecd12d2af2c5487e02381f74d
Kary01 <anakaren.ruizb@gmail.com>
2020-05-14 01:50:33 -0500

    cláusula where mysql

---
+++ 34-practica.php
@@ -0,0 +1,50 @@
+<?php
+
+    //La cláusula WHERE se usa para filtrar registros.
+
+    echo "<table style='border: solid 1px black;'>";
+    echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+    class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+    }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "prueba";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //seleccionamos id, firstname, lastname de MyGuests donde lastname es Doe
+    $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'");
+
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+    }
+    catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+    echo "</table>";
+?>
